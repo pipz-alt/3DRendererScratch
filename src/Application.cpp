1eb8e602771195e72b3c40c64ba92124d821a3b1
devin <pipzofficial247@gmail.com>
2026-09-03 13:06:15 -0400

    Matrix Transformations

--- src/Application.cpp
+++ src/Application.cpp
@@ -148,13 +148,16 @@ namespace Renderer
         mesh.rotation.z += 0.01;
 
         mesh.scale.x += 0.002;
-        mesh.translation.x += 0.002;
-        mesh.translation.y += 0.002;
-        mesh.translation.z += 0.002;
+        mesh.translation.x += 0.02;
+        mesh.translation.y += 0;
+        mesh.translation.z += 0;
 
         mat4_t scale_matrix = mat4_make_scale(mesh.scale.x, mesh.scale.y, mesh.scale.z);
+        mat4_t rotation_matrix_x = mat4_make_rotation_x(mesh.rotation.x);
+        mat4_t rotation_matrix_y = mat4_make_rotation_y(mesh.rotation.y);
+        mat4_t rotation_matrix_z = mat4_make_rotation_z(mesh.rotation.z);
         mat4_t translation_matrix = mat4_make_translation(mesh.translation.x, mesh.translation.y, mesh.translation.z);
-
+    
         int num_faces = mesh.faces.size();
         // Loop all triangle faces of our mesh
         for (int i = 0; i < num_faces; i++) {
@@ -173,6 +176,9 @@ namespace Renderer
 
                     transformed_vertex = mat4_mul_vec4(scale_matrix, transformed_vertex);
                     transformed_vertex = mat4_mul_vec4(translation_matrix, transformed_vertex);
+                    transformed_vertex = mat4_mul_vec4(rotation_matrix_x, transformed_vertex);
+                    transformed_vertex = mat4_mul_vec4(rotation_matrix_y, transformed_vertex);  
+                    transformed_vertex = mat4_mul_vec4(rotation_matrix_z, transformed_vertex);
 
                     // transformed_vertex = vec3_rotate_x(transformed_vertex, mesh.rotation.x);
                     // transformed_vertex = vec3_rotate_y(transformed_vertex, mesh.rotation.y);
--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -1,4 +1,5 @@
 #include "Matrix.h"
+#include <math.h>
 
 mat4_t mat4_identity() {
     mat4_t m = {{
@@ -25,6 +26,45 @@ mat4_t mat4_make_translation(float tx, float ty, float tz)
     m.m[0][3] = tx;
     m.m[1][3] = ty;
     m.m[2][3] = tz;
+    return m;
+}
+
+mat4_t mat4_make_rotation_x(float angle)
+{
+    mat4_t m = mat4_identity();
+
+    float c = cos(angle);
+    float s = sin(angle);
+    m.m[1][1] = c;
+    m.m[1][2] = -s;
+    m.m[2][1] = s;
+    m.m[2][2] = c;
+    return m;
+}
+
+mat4_t mat4_make_rotation_y(float angle)
+{
+    mat4_t m = mat4_identity();
+
+    float c = cos(angle);
+    float s = sin(angle);
+    m.m[0][0] = c;
+    m.m[0][2] = s;
+    m.m[2][0] = -s;
+    m.m[2][2] = c;
+    return m;
+}
+
+mat4_t mat4_make_rotation_z(float angle)
+{
+    mat4_t m = mat4_identity();
+
+    float c = cos(angle);
+    float s = sin(angle);
+    m.m[0][0] = c;
+    m.m[0][1] = -s;
+    m.m[1][0] = s;
+    m.m[1][1] = c;
     return m;
 }
 
